fd925037193877cd2793f0553db5d3e7b14c81da
jaden-2 <jedidiahamonia1@gmail.com>
2026-08-08 16:56:58 +0100

    Map struct was not the reason certificate was not being mapped. I created an incomplete bidirectional relationship between volunteer and volunteerCertificate

--- src/main/java/com/backend/givr/volunteer/service/VolunteerService.java
+++ src/main/java/com/backend/givr/volunteer/service/VolunteerService.java
@@ -130,12 +130,7 @@ public class VolunteerService {
 
     @Cacheable(cacheNames = "volunteerProfile", key = "#volunteerId")
     public VolunteerProfile getVolunteerProfile(String volunteerId){
-        Volunteer volunteer = getVolunteer(volunteerId);
-        System.out.println(volunteer.getCertificates().getFirst().getCertId());
-        List<VolunteerCertificateDto> certs = certMapper.toDto(volunteer.getCertificates());
-        var profile =  mapper.toProfile(volunteer);
-        profile.setCertificates(certs);
-        return profile;
+        return mapper.toProfile(getVolunteer(volunteerId));
     }
 
 

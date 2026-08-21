a3f285ae1535312ed0c1e233c1b7d86cdc2eb6c5
jaden-2 <jedidiahamonia1@gmail.com>
2026-08-21 15:05:36 +0100

    Added rest controller annotation to certificate verification controller

--- src/main/java/com/backend/givr/shared/controller/CertificateVerificationController.java
+++ src/main/java/com/backend/givr/shared/controller/CertificateVerificationController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/${api.version}/api/certificates")
+@RestController
 public class CertificateVerificationController {
     @Autowired
     private CertificateService service;
